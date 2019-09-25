264f3482a7b1b0a289b92039b911a4c1ffe1e61b
tommyhenry90 <t.henry@student.unsw.edu.au>
2019-09-25 23:54:45 +1000

    updated some of the testing

--- image_splitter.py
+++ image_splitter.py
@@ -2,32 +2,39 @@ import cv2 as cv
 import sys
 import os
 import math
-import numpy as np
 
 
 class ImageSplitter:
     def __init__(self, filename, directory, tile_size=256, num_levels=9):
         self.filename = filename
         self.directory = directory
+        self.sub_directory = "image_pyramid"
         self.tile_size = tile_size
         self.num_levels = num_levels
         self.img = None
         self.load_image(self.filename, self.directory)
+        self.tiles = []
 
     def load_image(self, filename, directory):
         self.img = cv.imread(directory + "/" + filename)
         return self.img
 
     def save_image(self, img, level, x, y):
-        name = str(level) + "_" + str(x) + "_" + str(y) + ".jpg"
-        cv.imwrite(self.directory + "/" + name, img)
+        sub_sub_directory = self.directory + "/" + self.sub_directory + "/" + str(level)
+        if not os.path.exists(sub_sub_directory):
+            os.makedirs(sub_sub_directory)
+        name = str(x) + "_" + str(y) + ".jpg"
+        cv.imwrite(sub_sub_directory + "/" + name, img)
         return True
 
     def downsize_image(self, img, factor=2):
         rows, cols, _channels = map(int, img.shape)
         return cv.pyrDown(img, dstsize=(cols // factor, rows // factor))
 
+    # TODO downsize original images first before tiling to avoid pixelation on higher levels
     def tile_image(self, tile_size=256, num_levels=9):
+        if not os.path.exists(self.directory + "/" + self.sub_directory):
+            os.makedirs(self.directory + "/" + self.sub_directory)
         rows, cols, _channels = map(int, self.img.shape)
         num_cols = math.ceil(cols/tile_size)
         num_rows = math.ceil(rows / tile_size)
@@ -40,11 +47,12 @@ class ImageSplitter:
         return True
 
     def recombine_tiles(self, level, num_x, num_y):
+        sub_sub_directory = self.directory + "/" + self.sub_directory + "/" + str(level)
         previous_rows = []
         for x in range(num_x):
             next_row = []
             for y in range(num_y):
-                next_image = cv.imread(self.directory + "/" + str(level) + "_" + str(x) + "_" + str(y) + ".jpg")
+                next_image = cv.imread(sub_sub_directory + "/" + str(x) + "_" + str(y) + ".jpg")
                 next_row.append(next_image)
             previous_rows.append(cv.vconcat(next_row))
         recombined_tiles = cv.hconcat(previous_rows)
@@ -60,7 +68,6 @@ def main(argv):
     directory = os.path.dirname(path_name)
     filename = os.path.basename(path_name)
     splitter = ImageSplitter(filename, directory)
-    print(splitter.img)
     splitter.tile_image()
 
 
--- test_image_splitter.py
+++ test_image_splitter.py
@@ -2,13 +2,13 @@ import unittest
 from image_splitter import ImageSplitter
 import numpy as np
 import cv2 as cv
+import math
 
 
 def create_image(width, height, rgb_color=(0, 0, 0)):
     """Create new image(numpy array) filled with certain color in RGB"""
     # Create black blank image
     image = np.zeros((height, width, 3), np.uint8)
-
     # Since OpenCV uses BGR, convert the color first
     color = tuple(reversed(rgb_color))
     # Fill image with color
@@ -33,15 +33,22 @@ class TestImageSplitter(unittest.TestCase):
         downsized_image = self.splitter.downsize_image(self.splitter.img, factor)
         original_dimensions = self.splitter.img.shape
         downsized_dimensions = downsized_image.shape
-        self.assertEqual(original_dimensions[0] / factor, downsized_dimensions[0])
-        self.assertEqual(original_dimensions[1] / factor, downsized_dimensions[1])
+        self.assertEqual(original_dimensions[0] // factor, downsized_dimensions[0])
+        self.assertEqual(original_dimensions[1] // factor, downsized_dimensions[1])
         self.assertEqual(original_dimensions[2], downsized_dimensions[2])
 
+    # TODO make this test more robust for different image sizes
     def test_tiles_create_original_image(self):
-        self.splitter.tile_image(1, 1)
-        recombined_tiles = self.splitter.recombine_tiles(0, 4, 4)
+        tile_size = 1
+        num_y, num_x, _channels = map(int, self.splitter.img.shape)
+        num_x = math.ceil(num_x / tile_size)
+        num_y = math.ceil(num_y / tile_size)
+        self.splitter.tile_image(tile_size, num_levels=1)
+        recombined_tiles = self.splitter.recombine_tiles(0, num_x, num_y)
         # Check images have same dimensions and channels
         self.assertEqual(recombined_tiles.shape, self.splitter.img.shape)
+
+        # Check images are exactly the same
         difference = cv.subtract(recombined_tiles, self.splitter.img)
         b, g, r = cv.split(difference)
         self.assertEqual(cv.countNonZero(b), 0)
